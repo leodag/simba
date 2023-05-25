411277caf52ebbc1b683ad77f1a73f66a918ea6d
Leonardo Dagnino <leodag.sch@gmail.com>
2023-05-25 10:13:58 -0300

    delete unneeded file (integrated into program)

--- acd-to-spinworks.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-for file in "$@"; do
-    grep -E '^Frequency|^[0-9-]' < "$file" | sed 's/Frequency = /ti: '"\n"'##frequency /g' | sed 's/\t/  /' | sed 's/,/./g' | tr -d '\015' > "$file.spin"
-done
